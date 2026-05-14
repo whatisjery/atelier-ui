980576b2ecfb9aaa47885dfef7fb60a69e8b22e8
Jérémie Nallet <jeremienallet@gmail.com>
2026-05-14 23:20:51 +0200

    refactor: skip scroll if URL has hash

--- src/components/common/ScrollRestorer.tsx
+++ src/components/common/ScrollRestorer.tsx
@@ -7,7 +7,9 @@ export function ScrollRestorer() {
     const pathname = usePathname()
 
     useEffect(() => {
-        window.scrollTo(0, 0)
+        if (!window.location.hash) {
+            window.scrollTo(0, 0)
+        }
     }, [pathname])
 
     return null
